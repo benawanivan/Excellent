63de281054aecbd13c15303b9c7ee80b79932a8e
AdamSukma <adamsukma10@gmail.com>
2021-01-23 04:21:51 +0700

    sidebar untuk hp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,64 +1,74 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-  <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets') }}/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="{{ asset('assets') }}/img/favicon.png">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <!-- Extra details for Live View on GitHub Pages -->
-  <title>
-    {{$namePage}}
-  </title>
-  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-  <!--     Fonts and icons     -->
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <!-- CSS Files -->
-  <link href="{{ asset('assets') }}/css/bootstrap.min.css" rel="stylesheet" />
-  <link href="{{ asset('assets') }}/css/now-ui-dashboard.css?v=1.3.0" rel="stylesheet" />
-  <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="{{ asset('assets') }}/demo/demo.css" rel="stylesheet" />
-  <!--   Core JS Files   -->
-  <script src="{{ asset('assets') }}/js/core/jquery.min.js"></script>
-  <script src="{{ asset('assets') }}/js/core/popper.min.js"></script>
-  <script src="{{ asset('assets') }}/js/core/bootstrap.min.js"></script>
-  <script src="{{ asset('assets') }}/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-  <script src="{{ asset('js/calendar.js') }}"></script>
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets') }}/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="{{ asset('assets') }}/img/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <!-- Extra details for Live View on GitHub Pages -->
+    <title>
+       {{$namePage}}
+    </title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
+        name='viewport' />
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <!-- CSS Files -->
+    <link href="{{ asset('assets') }}/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="{{ asset('assets') }}/css/now-ui-dashboard.css?v=1.3.0" rel="stylesheet" />
+    <!-- CSS Just for demo purpose, don't include it in your project -->
+    <link href="{{ asset('assets') }}/demo/demo.css" rel="stylesheet" />
+    <!--   Core JS Files   -->
+    <script src="{{ asset('assets') }}/js/core/jquery.min.js"></script>
+    <script src="{{ asset('assets') }}/js/core/popper.min.js"></script>
+    <script src="{{ asset('assets') }}/js/core/bootstrap.min.js"></script>
+    <script src="{{ asset('assets') }}/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+    <script src="{{ asset('js/calendar.js') }}"></script>
+    <script src="{{ asset('js/sidebar.js') }}"></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+
+
 </head>
 
-<body class="{{ $class ?? '' }}">
-  <div class="wrapper">
+<body class="body">
+
     @auth('admin')
-      @include('layouts.page_template.admin')
+        @include('layouts.page_template.admin')
     @endauth
     @auth('murid')
-      @include('layouts.page_template.murid')
+        @include('layouts.page_template.murid')
     @endauth
     @auth('guru')
-      @include('layouts.page_template.guru')
+        @include('layouts.page_template.guru')
     @endauth
     @auth('ortu')
-      @include('layouts.page_template.ortu')
+        @include('layouts.page_template.ortu')
     @endauth
     @guest
-      @include('layouts.page_template.guest')
+        @include('layouts.page_template.guest')
     @endguest
-  </div>
-  
-  <!--  Google Maps Plugin    -->
-  {{-- <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> --}}
-  <!-- Chart JS -->
-  <script src="{{ asset('assets') }}/js/plugins/chartjs.min.js"></script>
-  <!--  Notifications Plugin    -->
-  <script src="{{ asset('assets') }}/js/plugins/bootstrap-notify.js"></script>
-  <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="{{ asset('assets') }}/js/now-ui-dashboard.min.js?v=1.3.0" type="text/javascript"></script>
-  <!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
-  <script src="{{ asset('assets') }}/demo/demo.js"></script>
-   <script src="{{ asset('js/app.js') }}"></script>
-  @stack('js')
+
+    <script src="{{ asset('assets') }}/js/core/jquery.min.js"></script>
+    <script src="{{ asset('assets') }}/js/core/popper.min.js"></script>
+    <script src="{{ asset('assets') }}/js/core/bootstrap.min.js"></script>
+    <script src="{{ asset('js/sidebar.js') }}"></script>
+
+    <!--  Google Maps Plugin    -->
+    {{-- <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+    --}}
+    <!-- Chart JS -->
+    {{-- <script src="{{ asset('assets') }}/js/plugins/chartjs.min.js"></script> --}}
+    <!--  Notifications Plugin    -->
+    <script src="{{ asset('assets') }}/js/plugins/bootstrap-notify.js"></script>
+    <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="{{ asset('assets') }}/js/now-ui-dashboard.min.js?v=1.3.0" type="text/javascript"></script>
+    <!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
+    {{-- <script src="{{ asset('assets') }}/demo/demo.js"></script> --}}
+    <script src="{{ asset('js/app.js') }}"></script>
+    @stack('js')
 </body>
 
 </html>
